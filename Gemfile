80aff7226bf16ab85b74d7e169fe55bf30349161
Matt Whiteley <mwhiteley@engineyard.com>
2012-08-21 13:56:54 -0700

    Run some Vagrant commands

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem 'grape', :git => 'https://github.com/intridea/grape.git'
 gem 'puma'
 gem 'uuid'
+gem 'vagrant'
 
 group :development do
   gem 'shotgun'
--- fragrant.rb
+++ fragrant.rb
@@ -1,5 +1,6 @@
 require 'grape'
 require 'uuid'
+require 'vagrant'
 
 class Fragrant < Grape::API
   version 'v1', :using => :header, :vendor => 'nextgen'
@@ -9,6 +10,14 @@ class Fragrant < Grape::API
 
     def boxdir
       @boxdir ||= File.expand_path(File.join(File.dirname(__FILE__), 'vboxes'))
+    end
+
+    def boxname
+      @boxname ||= 'nextgen64'
+    end
+
+    def boxurl
+      @boxurl ||= 'http://shunterus.s3.amazonaws.com/nextgen64.box'
     end
 
     def envglob
@@ -22,30 +31,121 @@ class Fragrant < Grape::API
       UUID.generate
     end
 
+    def venv(id)
+      Vagrant::Environment.new({ :cwd => File.join(boxdir, id) })
+    end
+
   end
 
   resource :environments do
+
+    desc "Destroys a Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    delete '/destroy/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Destroy.new([], v)
+      cmd.execute
+      params[:id]
+    end
 
     desc "Lists Vagrant environments"
     get :list do
       envglob
     end
 
-    desc "Creates a Vagrant environment"
-    post :new do
-      machine = envrand
-      Dir.mkdir(File.join(boxdir, machine), 0755)
-      File.open(File.join(boxdir, machine, 'Vagrantfile'), 'w') {}
-      machine
-    end
-
-    desc "Removes a Vagrant environment"
+    desc "Halts Vagrant environment"
     params do
       # TODO: add regex to validate id
       requires :id, :desc => "Vagrant environment id", :type => String
     end
-    get '/rm/:id' do
-      FileUtils.remove_entry_secure(File.join(boxdir, params[:id])) if envglob.include?(params[:id])
+    post '/halt/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Halt.new([], v)
+      cmd.execute
+      params[:id]
+    end
+
+    desc "Initializes a Vagrant environment"
+    post :init do
+      machine = envrand
+      Dir.mkdir(File.join(boxdir, machine), 0755)
+      v = venv(machine)
+      cmd = Vagrant::Command::Init.new([], v)
+      cmd.execute
+      machine
+    end
+
+    desc "Provisions Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    post '/provision/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Provision.new([], v)
+      cmd.execute
+      params[:id]
+    end
+
+    desc "Reloads Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    post '/reload/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Reload.new([], v)
+      cmd.execute
+      params[:id]
+    end
+
+    desc "Resumes Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    post '/resume/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Resume.new([], v)
+      cmd.execute
+      params[:id]
+    end
+
+    desc "Prints status of Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    get '/status/:id' do
+      v = venv(params[:id])
+      { :state => v.vms[:default].state }
+    end
+
+    desc "Suspends Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    post '/suspend/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Suspend.new([], v)
+      cmd.execute
+      params[:id]
+    end
+
+    desc "Boots Vagrant environment"
+    params do
+      # TODO: add regex to validate id
+      requires :id, :desc => "Vagrant environment id", :type => String
+    end
+    post '/up/:id' do
+      v = venv(params[:id])
+      cmd = Vagrant::Command::Up.new([], v)
+      cmd.execute
+      params[:id]
     end
 
   end
